86c5cc993ed79fca8553812284190069e729d3ca
Andrei Nicoara <AndreiN95@gmail.com>
2018-09-11 18:45:21 +0300

    Modern opengl port.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,332 @@
-#include <GL/freeglut.h>
+#include <SDL.h>
+#include <glew.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 #include <math.h>
 #include <time.h>
 #include <vector>
-#include "mouse.h"
-#include "keyboard.h"
-#define FPS 60
+#include <iostream>
+//#include "mouse.h"
+//#include "keyboard.h"
 
 #define MAX_ACCELERATION  0.1f
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define MAX_FPS 60
+
+using namespace std;
+
+SDL_Window *mainWindow;
+SDL_GLContext mainContext;
+GLuint rendering_program;
+GLuint vertex_array_object;
+
+int check_program(GLuint program)
+{
+	GLint isLinked = 0;
+	glGetProgramiv(program, GL_LINK_STATUS, &isLinked);
+	if (isLinked == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> infoLog(maxLength);
+		glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+
+		// The program is useless now. So delete it.
+		glDeleteProgram(program);
+
+		std::cout << "ERROR: ";
+		for (auto i : infoLog)
+		{
+			std::cout << i;
+		}
+		// Provide the infolog in whatever manner you deem best.
+		// Exit with failure.
+		return 0;
+	}
+	return 1;
+}
+
+GLuint compile_shaders(void)
+{
+	GLuint vertex_shader;
+	GLuint fragment_shader;
+	GLuint tesselation_control_shader;
+	GLuint tesselation_evaluation_shader;
+	GLuint geometry_shader;
+	GLuint program;
+
+	static const GLchar *vertex_shader_source[] =
+	{
+		"#version 450 core \n"
+		"\n"
+		"layout (location = 0) in vec4 offset; \n"
+		"layout (location = 1) in vec4 color; \n"
+		"out vec4 vs_color; \n"
+		"\n"
+		"void main(void) \n"
+		"{ \n"
+		"   const vec4 vertices[3] = vec4[3](vec4( 0.25, -0.25, 0.5, 1.0), \n"
+		"									 vec4( -0.25, -0.25, 0.5, 1.0), \n"
+		"									 vec4( 0.25, 0.25, 0.5, 1.0)); \n"
+		"   const vec4 colors[3] = vec4[3](vec4( 1.0, 0.0, 0.0, 1.0), \n"
+		"									 vec4( 0.0, 1.0, 0.0, 1.0), \n"
+		"									 vec4( 0.0, 0.0, 1.0, 1.0)); \n"
+		"\n"
+		"   gl_Position = vertices[gl_VertexID]+offset; \n"
+		"   vs_color = colors[gl_VertexID]; \n"
+		"} \n"
+	};
+
+	static const GLchar *tesselation_control_shader_source[] =
+	{
+		"#version 450 core \n"
+		"\n"
+		"layout (vertices = 3) out; \n"
+		"\n"
+		"in vec4 vs_color[]; \n"
+		"patch out vec4 patch_color; \n"
+		"void main(void) \n"
+		"{ \n"
+		"   if (gl_InvocationID == 0) \n"
+		"	{ \n"
+		"		gl_TessLevelInner[0] = 5.0; \n"
+		"		gl_TessLevelOuter[0] = 5.0; \n"
+		"		gl_TessLevelOuter[1] = 5.0; \n"
+		"		gl_TessLevelOuter[2] = 5.0; \n"
+		"	} \n"
+		"	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position; \n"
+		"	patch_color = vs_color[gl_InvocationID]; \n"
+		"} \n"
+	};
+
+	static const GLchar *tesselation_evaluation_shader_source[] =
+	{
+		"#version 450 core \n"
+		"\n"
+		"layout (triangles, equal_spacing, cw) in; \n"
+		"\n"
+		"patch in vec4 patch_color; \n"
+		"out vec4 tes_color; \n"
+		"void main(void) \n"
+		"{ \n"
+		"	gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position + \n"
+		"				   gl_TessCoord.y * gl_in[1].gl_Position + \n"
+		"				   gl_TessCoord.z * gl_in[2].gl_Position); \n"
+		"	tes_color = patch_color; \n"
+		"} \n"
+	};
+
+	static const GLchar *geometry_shader_source[] =
+	{
+		"#version 450 core \n"
+		"\n"
+		"layout (triangles) in; \n"
+		"layout (points, max_vertices = 3) out; \n"
+		"\n"
+		"in vec4 tes_color[]; \n"
+		"out vec4 gs_color; \n"
+		"void main(void) \n"
+		"{ \n"
+		"	int i; \n"
+		"	\n"
+		"	for (i = 0; i < gl_in.length(); i++) \n"
+		"	{ \n"
+		"		gs_color = tes_color[i]; \n"
+		"		gl_Position = gl_in[i].gl_Position; \n"
+		"		EmitVertex(); \n"
+		"	} \n"
+		"} \n"
+	};
+
+	static const GLchar *fragment_shader_source[] =
+	{
+		"#version 450 core \n"
+		"\n"
+		"in vec4 vs_color; \n"
+		"out vec4 color; \n"
+		"\n"
+		"void main(void) \n"
+		"{ \n"
+		"   color =  vs_color; \n"
+		"} \n"
+	};
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+
+	tesselation_control_shader = glCreateShader(GL_TESS_CONTROL_SHADER);
+	glShaderSource(tesselation_control_shader, 1, tesselation_control_shader_source, NULL);
+	glCompileShader(tesselation_control_shader);
+
+	tesselation_evaluation_shader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+	glShaderSource(tesselation_evaluation_shader, 1, tesselation_evaluation_shader_source, NULL);
+	glCompileShader(tesselation_evaluation_shader);
+
+	geometry_shader = glCreateShader(GL_GEOMETRY_SHADER);
+	glShaderSource(geometry_shader, 1, geometry_shader_source, NULL);
+	glCompileShader(geometry_shader);
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	//glAttachShader(program, tesselation_control_shader);
+	//glAttachShader(program, tesselation_evaluation_shader);
+	//glAttachShader(program, geometry_shader);
+	glAttachShader(program, fragment_shader);
+
+	glLinkProgram(program);
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(tesselation_control_shader);
+	glDeleteShader(tesselation_evaluation_shader);
+	glDeleteShader(geometry_shader);
+	glDeleteShader(fragment_shader);
+
+	if (!check_program(program))
+	{
+		return 0;
+	}
+
+	return program;
+}
+
+void SetOpenGLAttributes()
+{
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+}
+
+void CheckSDLError(int line = -1)
+{
+	std::string error = SDL_GetError();
+
+	if (error != "")
+	{
+		std::cout << "SLD Error : " << error << std::endl;
+
+		if (line != -1)
+			std::cout << "\nLine : " << line << std::endl;
+
+		SDL_ClearError();
+	}
+}
+
+bool Init()
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
+	}
+	else
+	{
+		cout << "SDL initialization succeeded!\n";
+	}
+
+	mainWindow = SDL_CreateWindow("Simple program", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+
+	if (!mainWindow)
+	{
+		std::cout << "Unable to create window\n";
+		CheckSDLError(__LINE__);
+		return false;
+	}
+
+	mainContext = SDL_GL_CreateContext(mainWindow);
+
+	SetOpenGLAttributes();
+
+	SDL_GL_SetSwapInterval(1);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	rendering_program = compile_shaders();
+
+	if (!rendering_program)
+	{
+		return false;
+	}
+
+	glCreateVertexArrays(1, &vertex_array_object);
+	glBindVertexArray(vertex_array_object);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPatchParameteri(GL_PATCH_VERTICES, 3);
+
+	return true;
+}
+
+void RunGame()
+{
+	bool loop = true;
+	unsigned int currentTime;
+	int frameTime;
+	const int frameDelay = 1000 / MAX_FPS;
+
+	while (loop)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				loop = false;
+			}
+		}
+
+		currentTime = SDL_GetTicks();
+		std::cout << (float)cos(currentTime / 1000) * 0.5f + 0.5f << std::endl;
+		const GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+		const GLfloat color[] = { (float)sin(currentTime / 1000) * 0.5f + 0.5f, (float)cos(currentTime / 1000) * 0.5f + 0.5f, 0.0f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, black);
+
+		glUseProgram(rendering_program);
+
+		GLfloat attrib[] = { (float)sin(currentTime / 1000) * 0.5f,
+			(float)cos(currentTime / 1000) * 0.6f,
+			0.0f, 0.0f };
+
+		glVertexAttrib4fv(0, attrib);
+		glVertexAttrib4fv(1, color);
+
+		glPointSize(5.0f);
+		//for the tesselation thing
+		//glDrawArrays(GL_PATCHES, 0, 3);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		GLenum e = glGetError();
+		SDL_GL_SwapWindow(mainWindow);
+
+		frameTime = SDL_GetTicks() - currentTime;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
+	}
+}
+
+void Cleanup()
+{
+	glDeleteVertexArrays(1, &vertex_array_object);
+	glDeleteProgram(rendering_program);
+	glDeleteVertexArrays(1, &vertex_array_object);
+
+	SDL_GL_DeleteContext(mainContext);
+	SDL_DestroyWindow(mainWindow);
+	SDL_Quit();
+}
 
 float randomClamped() //return a float number between -1 and 1
 {
@@ -67,8 +385,8 @@ public:
     };
     void show()
     {
-        glColor3f(_r,_g,_b);
-        glutSolidCube(2); //cub cu latura 2
+        //glColor3f(_r,_g,_b);
+        //glutSolidCube(2); //cub cu latura 2
     }
 
     void tag()
@@ -193,7 +511,7 @@ public:
     void moveTo(float px, float py)
     {
         this->setColor(1.0f,0.5f,0.0f);
-        glPushMatrix();
+        //glPushMatrix();
 
         //the distance between this body and the point
         double distX = px - this->x;
@@ -226,14 +544,14 @@ public:
 
 
 
-        glPopMatrix();
+        //glPopMatrix();
 
         //spawn a red cube indicating the point
-        glPushMatrix();
-        glColor3f(1.0f,0.0f,0.0f);
-        glTranslatef(px,py,-100.0f);
-        glutSolidCube(2);
-        glPopMatrix();
+        //glPushMatrix();
+        //glColor3f(1.0f,0.0f,0.0f);
+        //glTranslatef(px,py,-100.0f);
+        //glutSolidCube(2);
+        //glPopMatrix();
 
     }
     float _r,_g,_b;
@@ -247,198 +565,207 @@ bool wanderOn = false, followOn = false, cohesionOn = false;
 
 void initGL(int width, int height)
 {
-    const GLfloat light_ambient[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-    const GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+    //const GLfloat light_ambient[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    //const GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    //const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    //const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
 
-    glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHTING);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    //glEnable(GL_LIGHT0);
+    //glEnable(GL_LIGHTING);
+    //glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    //glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    //glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    //glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+    //glEnable(GL_CULL_FACE);
+    //glCullFace(GL_BACK);
 
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
+    //glEnable(GL_DEPTH_TEST);
+    //glDepthFunc(GL_LESS);
 
-    glEnable(GL_COLOR_MATERIAL);
+    //glEnable(GL_COLOR_MATERIAL);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0f, (GLfloat)width/(GLfloat)height, 2.0f, 100.0f);
-    glMatrixMode(GL_MODELVIEW);
+    //glMatrixMode(GL_PROJECTION);
+    //glLoadIdentity();
+    //gluPerspective(45.0f, (GLfloat)width/(GLfloat)height, 2.0f, 100.0f);
+    //glMatrixMode(GL_MODELVIEW);
 
-    // init bodies
-    shark.x = 0.0f;
-    shark.y = 0.0f;
-    shark.z = -100.0f;
-    shark.setColor(1.0f,0.0f,0.0f);
+    //// init bodies
+    //shark.x = 0.0f;
+    //shark.y = 0.0f;
+    //shark.z = -100.0f;
+    //shark.setColor(1.0f,0.0f,0.0f);
 
-    body n = body(10.0f,-5.0f,-100.f);
-    n.setColor(0.0f,0.0f,1.0f);
+    //body n = body(10.0f,-5.0f,-100.f);
+    //n.setColor(0.0f,0.0f,1.0f);
 
-    neighbours.push_back(n);
-
-
-}
-static void display(void)
-{
-    static int frame,timebase=0;
-    int time;
-    char s[100];
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glLoadIdentity(); //initializarea sistemului de coordonate
-
-        shark.moveTo(shark._targetX,shark._targetY);
-        shark.tagNeighbors(neighbours,1.0f);
-        shark.separation(neighbours);
-
-
-    for (std::vector<body>::iterator it = neighbours.begin() ; it != neighbours.end(); ++it)
-    {
-
-        glPushMatrix();
-        glTranslatef(it->x, it->y, it->z);
-        it->show();
-
-        if(!it->isMoving && wanderOn && it==neighbours.begin())
-        {
-
-            it->wander(300.0f, 200.0f,200.0f);
-        }
-        else if(!it->isMoving && followOn )
-        {
-            it->offsetPursuit(neighbours.front(),2.0f,2.0f);
-
-        }
-        else if(!it->isMoving && cohesionOn)
-        {
-            it->cohesion(neighbours);
-        }
-        else if(!it->isMoving && wanderOn)
-        {
-            it->wander(300.0f,200.0f,200.0f);
-        }
-        if(it->isTagged())
-        {
-            it->moveMe();
-        }
-        else if(wanderOn || cohesionOn || followOn)
-        {
-            it->moveTo(it->_targetX,it->_targetY);
-        }
-        glPopMatrix();
-    }
-
-
-    glutSwapBuffers();
-
-}
-
-static void timer(int v)
-{
-    static int frame,timebase=0;
-    int time;
-    char s[100];
-    frame++;
-    time=glutGet(GLUT_ELAPSED_TIME);
-    if(Keyboard::isPressed('w'))
-    {
-        wanderOn = true;
-        printf("wander activated\n");
-    }
-    if(Keyboard::isPressed('s'))
-    {
-        wanderOn = false;
-        printf("wander deactivated\n");
-    }
-    if(Keyboard::isPressed('q'))
-    {
-        followOn = true;
-        printf("follow activated");
-    }
-    if(Keyboard::isPressed('a'))
-    {
-        followOn = false;
-        printf("follow deactivated");
-    }
-
-    if(Keyboard::isPressed('d'))
-    {
-
-        cohesionOn = false;
-        printf("cohesion deactivated");
-    }
-    if(Keyboard::isPressed('e'))
-    {
-        cohesionOn = true;
-        printf("cohesion activated");
-    }
-    if(Mouse::right_isPressed())
-    {
-        shark._targetX = (float)Mouse::getX()/((float)glutGet(GLUT_WINDOW_WIDTH)/108.0f)-54;
-        shark._targetY = -1*((float)Mouse::getY()/((float)glutGet(GLUT_WINDOW_HEIGHT)/80.0f)-40);
-    }
-
-    if(Mouse::left_isPressed())
-    {
-
-        float x = (float)Mouse::getX()/((float)glutGet(GLUT_WINDOW_WIDTH)/108.0f)-54;
-        float y = -1*((float)Mouse::getY()/((float)glutGet(GLUT_WINDOW_HEIGHT)/80.0f)-40);
-        body n = body(x,y,-100.f);
-        n.setColor(0.0f,0.0f,1.0f);
-
-        neighbours.push_back(n);
-    }
-
-    if (time - timebase > 0)
-    {
-        sprintf(s,"[FPS:%4.2f] Lab 2: Imagine+Imagine+Imagine+...= Animatie !",frame*1000.0/(time-timebase));
-        glutSetWindowTitle(s);
-        timebase = time;
-        frame = 0;
-    }
-
-    glutPostRedisplay();
-    glutTimerFunc(1000/FPS,timer,1);
+    //neighbours.push_back(n);
 
 
 }
 
-static void
-idle(void)
-{
-    glutPostRedisplay();
+//static void display(void)
+//{
+//    static int frame,timebase=0;
+//    int time;
+//    char s[100];
+//
+//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//    glLoadIdentity(); //initializarea sistemului de coordonate
+//
+//        shark.moveTo(shark._targetX,shark._targetY);
+//        shark.tagNeighbors(neighbours,1.0f);
+//        shark.separation(neighbours);
+//
+//
+//    for (std::vector<body>::iterator it = neighbours.begin() ; it != neighbours.end(); ++it)
+//    {
+//
+//        glPushMatrix();
+//        glTranslatef(it->x, it->y, it->z);
+//        it->show();
+//
+//        if(!it->isMoving && wanderOn && it==neighbours.begin())
+//        {
+//
+//            it->wander(300.0f, 200.0f,200.0f);
+//        }
+//        else if(!it->isMoving && followOn )
+//        {
+//            it->offsetPursuit(neighbours.front(),2.0f,2.0f);
+//
+//        }
+//        else if(!it->isMoving && cohesionOn)
+//        {
+//            it->cohesion(neighbours);
+//        }
+//        else if(!it->isMoving && wanderOn)
+//        {
+//            it->wander(300.0f,200.0f,200.0f);
+//        }
+//        if(it->isTagged())
+//        {
+//            it->moveMe();
+//        }
+//        else if(wanderOn || cohesionOn || followOn)
+//        {
+//            it->moveTo(it->_targetX,it->_targetY);
+//        }
+//        glPopMatrix();
+//    }
+//
+//
+//    glutSwapBuffers();
+//
+//}
 
-}
-/* Punct de intrare in program */
-int
-main(int argc, char *argv[])
-{
-    int width = 640;
-    int height = 480;
+//static void timer(int v)
+//{
+//    static int frame,timebase=0;
+//    int time;
+//    char s[100];
+//    frame++;
+//    time=glutGet(GLUT_ELAPSED_TIME);
+//    if(Keyboard::isPressed('w'))
+//    {
+//        wanderOn = true;
+//        printf("wander activated\n");
+//    }
+//    if(Keyboard::isPressed('s'))
+//    {
+//        wanderOn = false;
+//        printf("wander deactivated\n");
+//    }
+//    if(Keyboard::isPressed('q'))
+//    {
+//        followOn = true;
+//        printf("follow activated");
+//    }
+//    if(Keyboard::isPressed('a'))
+//    {
+//        followOn = false;
+//        printf("follow deactivated");
+//    }
+//
+//    if(Keyboard::isPressed('d'))
+//    {
+//
+//        cohesionOn = false;
+//        printf("cohesion deactivated");
+//    }
+//    if(Keyboard::isPressed('e'))
+//    {
+//        cohesionOn = true;
+//        printf("cohesion activated");
+//    }
+//    if(Mouse::right_isPressed())
+//    {
+//        shark._targetX = (float)Mouse::getX()/((float)glutGet(GLUT_WINDOW_WIDTH)/108.0f)-54;
+//        shark._targetY = -1*((float)Mouse::getY()/((float)glutGet(GLUT_WINDOW_HEIGHT)/80.0f)-40);
+//    }
+//
+//    if(Mouse::left_isPressed())
+//    {
+//
+//        float x = (float)Mouse::getX()/((float)glutGet(GLUT_WINDOW_WIDTH)/108.0f)-54;
+//        float y = -1*((float)Mouse::getY()/((float)glutGet(GLUT_WINDOW_HEIGHT)/80.0f)-40);
+//        body n = body(x,y,-100.f);
+//        n.setColor(0.0f,0.0f,1.0f);
+//
+//        neighbours.push_back(n);
+//    }
+//
+//    if (time - timebase > 0)
+//    {
+//        sprintf(s,"[FPS:%4.2f] Lab 2: Imagine+Imagine+Imagine+...= Animatie !",frame*1000.0/(time-timebase));
+//        glutSetWindowTitle(s);
+//        timebase = time;
+//        frame = 0;
+//    }
+//
+//    glutPostRedisplay();
+//    glutTimerFunc(1000/FPS,timer,1);
+//
+//
+//}
 
-    srand(time(NULL));
-    glutInit(&argc, argv);
-    glutInitWindowSize(width,height);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow(""); //titlu vid, il setez in display()
-    glutKeyboardFunc(Keyboard::keyDown);
-    glutKeyboardUpFunc(Keyboard::keyUp);
-    glutMotionFunc(Mouse::updatePosition);
-    glutPassiveMotionFunc(Mouse::updatePosition);
-    glutMouseFunc(Mouse::updateButton);
-    glutDisplayFunc(display);
-    //glutIdleFunc(idle);
-    glutTimerFunc(1000/FPS,timer,1);
-    initGL(width, height);
-    glutMainLoop();
-    return EXIT_SUCCESS;
+//static void idle(void)
+//{
+//    glutPostRedisplay();
+//
+//}
+
+int main(int argc, char *argv[])
+{
+    //int width = 640;
+    //int height = 480;
+
+    //srand(time(NULL));
+    //glutInit(&argc, argv);
+    //glutInitWindowSize(width,height);
+    //glutInitWindowPosition(10,10);
+    //glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    //glutCreateWindow(""); //titlu vid, il setez in display()
+    //glutKeyboardFunc(Keyboard::keyDown);
+    //glutKeyboardUpFunc(Keyboard::keyUp);
+    //glutMotionFunc(Mouse::updatePosition);
+    //glutPassiveMotionFunc(Mouse::updatePosition);
+    //glutMouseFunc(Mouse::updateButton);
+    //glutDisplayFunc(display);
+    ////glutIdleFunc(idle);
+    //glutTimerFunc(1000/FPS,timer,1);
+    //initGL(width, height);
+    //glutMainLoop();
+	if (!Init())
+	{
+		cin.get();
+		return -1;
+	}
+
+	RunGame();
+
+	Cleanup();
+	cin.get();
+	return 0;
 }
